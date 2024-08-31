23eb82e25c0878d280f3dc3549b10c460c52f953
Kyle Cubit <91747254+kjcubit47@users.noreply.github.com>
2024-08-30 21:24:55 -0500

    Create basic_calculations.cpp

---
+++ BasicExamples/basic_calculations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a = 5, b = 10;
+    std::cout << "Addition: " << a + b << std::endl;
+    std::cout << "Subtraction: " << b - a << std::endl;
+    std::cout << "Multiplication: " << a * b << std::endl;
+    std::cout << "Division: " << b / a << std::endl;
+    return 0;
+}
