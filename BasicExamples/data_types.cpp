cd3f0fe9b48704277d77f6906abc09dc7c5490a6
Kyle Cubit <91747254+kjcubit47@users.noreply.github.com>
2024-08-30 21:25:04 -0500

    Create data_types.cpp

---
+++ BasicExamples/data_types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int integerVal = 10;
+    double doubleVal = 20.5;
+    char charVal = 'A';
+    std::string stringVal = "Hello, C++!";
+
+    std::cout << "Integer: " << integerVal << std::endl;
+    std::cout << "Double: " << doubleVal << std::endl;
+    std::cout << "Character: " << charVal << std::endl;
+    std::cout << "String: " << stringVal << std::endl;
+
+    return 0;
+}
